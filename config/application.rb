1a1e9f15b97783a0ccbd4c072c9ee4c07ec4deac
cuongncFramgia <nguyen.chi.cuong@framgia.com>
2017-08-15 19:26:56 +0700

    login_gem_declare

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,8 @@ module TopLawyer
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+    config.i18n.load_path += Dir["#{Rails.root.to_s}/config/locales/**/*.{rb,yml}"]
+    config.i18n.default_locale = :en
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :accounts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "top_pages#index"
 end
