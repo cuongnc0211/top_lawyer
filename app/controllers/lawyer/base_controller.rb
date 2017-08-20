01dee73a328c16f564ffa82c3471d33af6efc693
cuongncFramgia <nguyen.chi.cuong@framgia.com>
2017-08-20 20:37:03 +0700

    test

--- app/controllers/lawyer/base_controller.rb
+++ app/controllers/lawyer/base_controller.rb
@@ -1,3 +1,9 @@
 class BaseController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, :authenticate_lawyer
+
+  private
+  def authenticate_lawyer
+  	flash[:danger] = t ".access_denies"
+    redirect_to root_path unless current_account.Lawyer?
+  end
 end
