3b779730af797a669f3dba43e52cdc7a2b617f3c
cuongncFramgia <nguyen.chi.cuong@framgia.com>
2017-08-22 20:59:12 +0700

    fake_du_lieu

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,55 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts "create_admin"
+10.times do |n|
+  Account.create email: "admin#{n + 1}@example.com",
+    name: "admin#{n+1}",
+    password: "12345678",
+    role: 0
+end
+
+puts "create_user"
+20.times do |n|
+  Account.create email: "user#{n+1}@example.com",
+    name: Faker::Name.name,
+    password: "12345678",
+    role: 1
+end
+
+
+puts "create_lawyer"
+20.times do |n|
+  account = Account.create(email: "lawyer#{n+1}@example.com",
+    name: Faker::Name.name,
+    password: "12345678",
+    role: 2)
+  account.create_lawyer_profile(point: Array(1..1000).sample,
+    lawyer_id: Faker::Lorem.characters(10),
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.cell_phone,
+    is_active: true,
+    is_manager: [0,1].sample)
+end
+
+puts "create_category"
+13.times do |n|
+  Category.create name: Faker::Lorem.words
+end
+
+puts "create_question"
+user_ids = Account.User.all.pluck :id
+category_ids = Category.all.pluck :id
+50.times do |n|
+  Question.create account_id: user_ids.sample,
+    title: Faker::Lorem.sentence(3),
+    content: Faker::Lorem.paragraph(2),
+    category_id: category_ids.sample
+end
+
+puts "create_article"
+50.times do |n|
+  Article.create account_id: user_ids.sample,
+    title: Faker::Lorem.sentence(3),
+    content: Faker::Lorem.paragraph(2),
+    category_id: category_ids.sample,
+    status: [0,1].sample,
+    total_vote: Array(1..50).sample
+end
