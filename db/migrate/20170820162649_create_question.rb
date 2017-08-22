23ac2aadb589c2e7fb3225245caf823d0e772b06
cuongncFramgia <nguyen.chi.cuong@framgia.com>
2017-08-22 19:16:07 +0700

    hot_fix_db

--- db/migrate/20170820162649_create_question.rb
+++ db/migrate/20170820162649_create_question.rb
@@ -1,10 +1,10 @@
 class CreateQuestion < ActiveRecord::Migration[5.1]
   def change
     create_table :questions do |t|
-      t.references :accounts
+      t.references :account
       t.string :title
       t.text :content
-      t.references :categories
+      t.references :category
 
       t.timestamps null: false
     end
