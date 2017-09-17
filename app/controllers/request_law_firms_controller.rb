3b5b8169da47d41effc54c1303243ea3b3a1b415
anhvmz <anhvmz@gmail.com>
2017-09-17 16:43:09 +0700

    request_law_firm

---
+++ app/controllers/request_law_firms_controller.rb
@@ -0,0 +1,16 @@
+class RequestLawFirmsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    @request_lf = current_account.request_law_firms.new
+    @request_lf.law_firm_id = params[:law_firm_id]
+    if @request_lf.save
+      redirect_to law_firm_path(params[:law_firm_id])
+      flash[:success] = t ".new"
+    else
+      flash.now[:alert] = t ".comment_error"
+    end
+  end
+end
