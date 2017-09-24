39b692fe3b9bf12a72a0b45cc3e97f9d4bf4aca0
cuongncFramgia <nguyen.chi.cuong@framgia.com>
2017-09-24 12:27:58 +0700

    aws_s3

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,9 +1,8 @@
 class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::RMagick
   include CarrierWave::MiniMagick
-
-  storage :file
-
+  require "carrierwave/storage/fog"
+  
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
