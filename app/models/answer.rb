8170e09e116a5b089a40e453447312ceb2b75155
anhvmz <anhvmz@gmail.com>
2017-10-09 07:15:12 +0700

    add comment for answer

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,6 +1,7 @@
 class Answer < ApplicationRecord
   belongs_to :account
   belongs_to :question
+  has_many :comments, as: :commentable
 
   ANSWER_ATTRIBUTES = [:content, :total_vote, :question_id]
 end
