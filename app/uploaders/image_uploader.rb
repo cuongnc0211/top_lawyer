cc3c7064084aecf8b4c0ff85f0ae025fd93db10b
cuongncFramgia <nguyen.chi.cuong@framgia.com>
2017-11-21 21:42:47 +0700

    hot_fix

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,8 +1,7 @@
 class ImageUploader < CarrierWave::Uploader::Base
-  include CarrierWave::RMagick
   include CarrierWave::MiniMagick
   require "carrierwave/storage/fog"
-  
+
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
