de29b9d9ed449515e5eb9aa5322b93e7d34e25aa
vietanhtvt <vietanhtvt@gmail.com>
2017-11-28 11:02:46 +0700

    UI_questions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,10 +1,10 @@
 class QuestionsController < ApplicationController
   impressionist actions: [:show], unique: [:session_hash]
-  
+
   def index
     @questions = Question.all.order(created_at: :desc).page(params[:page])
       .per Settings.article.top_page.per
-    @ranking_lawyers ||= Account.top_lawyer Settings.ranking.top_page
+    @top_lawyers ||= Account.top_lawyer Settings.ranking.top_page
   end
 
   def show
