82db16ead81c896b734ad504387a04956ec96585
cuongncFramgia <nguyen.chi.cuong@framgia.com>
2017-12-19 19:50:48 +0700

    deploy_10

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -111,9 +111,4 @@ Rails.application.configure do
    :enable_starttls_auto => true
   }
   config.action_cable.disable_request_forgery_protection = true
-
-  if ENV["CDN_ASSET"] == "true"
-    config.assets.prefix = "assets"
-    config.action_controller.asset_host = ENV["CDN_ASSET_HOST"]
-  end
 end
